93d20cc9a61e6bbfc4755f99aa857c50cf8b4c21
Ben Ling <belinghy@gmail.com>
2019-07-12 14:34:20 -0700

    Remove unused envs

--- mocca_envs/__init__.py
+++ mocca_envs/__init__.py
@@ -48,27 +48,3 @@ register(
     entry_point="mocca_envs.walker3d_envs:Walker3DCustomEnv",
     max_episode_steps=1000,
 )
-
-register(
-    id="Child3DCustomEnv-v0",
-    entry_point="mocca_envs.walker3d_envs:Child3DCustomEnv",
-    max_episode_steps=1000,
-)
-
-register(
-    id="Walker3DChairEnv-v0",
-    entry_point="mocca_envs.walker3d_envs:Walker3DChairEnv",
-    max_episode_steps=1000,
-)
-
-register(
-    id="Walker3DStepperEnv-v0",
-    entry_point="mocca_envs.walker3d_envs:Walker3DStepperEnv",
-    max_episode_steps=1000,
-)
-
-register(
-    id="Walker3DMimicEnv-v0",
-    entry_point="mocca_envs.imitation.imitation_envs:Walker3DMimicEnv",
-    max_episode_steps=1000,
-)
