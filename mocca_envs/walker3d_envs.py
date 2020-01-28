f0d92435eb67fc31381bbb8b6c394f0e9d790fa6
Ben Ling <belinghy@gmail.com>
2020-01-27 16:26:12 -0800

    Finish basic version of TerrainEnv

--- mocca_envs/walker3d_envs.py
+++ mocca_envs/walker3d_envs.py
@@ -16,6 +16,7 @@ from mocca_envs.bullet_objects import (
 )
 from mocca_envs.robots import Child3D, Walker3D
 
+
 Colors = {
     "dodgerblue": (0.11764705882352941, 0.5647058823529412, 1.0, 1.0),
     "crimson": (0.8627450980392157, 0.0784313725490196, 0.23529411764705882, 1.0),
@@ -706,6 +707,12 @@ class Walker3DTerrainEnv(EnvBase):
     llc_frame_skip = 1
     sim_frame_skip = 4
 
+    electricity_cost = 4.5
+    stall_torque_cost = 0.225
+    joints_at_limit_cost = 0.1
+
+    termination_height = 0.7
+
     def __init__(self, render=False):
         self.terrain_size = (256, 256)
         self.vision_hsize = 2
@@ -713,53 +720,140 @@ class Walker3DTerrainEnv(EnvBase):
         super().__init__(Walker3D, render, remove_ground=True)
         self.robot.set_base_pose(pose="running_start")
 
-        # robot_state + (2 targets) * (x, y, z, x_tilt, y_tilt)
-        high = np.inf * np.ones(self.robot.observation_space.shape[0])
+        # robot_state + (2 targets) + vision
+        map_size = (self.vision_hsize * 2 + 1) ** 2
+        high = np.inf * np.ones(self.robot.observation_space.shape[0] + 2 + map_size)
         self.observation_space = gym.spaces.Box(-high, high, dtype=np.float32)
         self.action_space = self.robot.action_space
 
     def create_terrain(self):
         self.terrain = HeightField(self._p, self.terrain_size)
         self.ground_ids = {(self.terrain, -1)}
-
         self.terrain.reload()
 
+    def get_observation_components(self):
+        sin_ = self.distance_to_target * np.sin(self.angle_to_target)
+        sin_ = sin_ / (1 + abs(sin_))
+        cos_ = self.distance_to_target * np.cos(self.angle_to_target)
+        cos_ = cos_ / (1 + abs(cos_))
+
+        return (
+            self.robot_state,
+            [sin_],
+            [cos_],
+            [self.robot.body_rpy[2]],
+            self.vision_map.flatten(),
+        )
+
     def reset(self):
+        if self.is_render:
+            self._p.configureDebugVisualizer(self._p.COV_ENABLE_RENDERING, 0)
+
         self.done = False
 
         self._p.restoreState(self.state_id)
-
         self.terrain.reload()
-
         self.robot_state = self.robot.reset(pos=(0, 0, 1.28))
-        self.calc_local_height_map()
+
+        self.walk_target = np.array((*self.terrain_size, 0)) / 2
+        self.vision_map = self.calc_local_height_map()
 
         # Reset camera
         if self.is_render:
             self.camera.lookat(self.robot.body_xyz)
 
-        return self.robot_state
+        self.calc_potential()
+
+        state = np.concatenate(self.get_observation_components())
+
+        if self.is_render:
+            self._p.configureDebugVisualizer(self._p.COV_ENABLE_RENDERING, 1)
+
+        return state
 
     def step(self, action):
 
         self.robot.apply_action(action)
         self.scene.global_step()
 
-        # Don't calculate the contacts for now
-        self.robot_state = self.robot.calc_state()
-        self.calc_local_height_map()
+        self.robot_state = self.robot.calc_state(self.ground_ids)
+        self.vision_map = self.calc_local_height_map()
 
-        reward = 0
+        self.calc_env_state(action)
+
+        reward = self.progress - self.energy_penalty
+        reward += self.tall_bonus - self.posture_penalty - self.joints_penalty
+
+        state = np.concatenate(self.get_observation_components())
 
         if self.is_render:
             self._handle_keyboard()
             self.camera.track(pos=self.robot.body_xyz)
 
-        return self.robot_state, reward, self.done, {}
+        return state, reward, self.done, {}
 
-    def calc_feet_state(self):
-        for i, f in enumerate(self.robot.feet):
-            self.robot.feet_xyz[i] = f.pose().xyz()
+    def calc_potential(self):
+
+        walk_target_theta = np.arctan2(
+            self.walk_target[1] - self.robot.body_xyz[1],
+            self.walk_target[0] - self.robot.body_xyz[0],
+        )
+        walk_target_delta = self.walk_target - self.robot.body_xyz
+
+        self.angle_to_target = walk_target_theta - self.robot.body_rpy[2]
+
+        self.distance_to_target = (
+            walk_target_delta[0] ** 2 + walk_target_delta[1] ** 2
+        ) ** (1 / 2)
+
+        self.linear_potential = -self.distance_to_target / self.scene.dt
+        self.angular_potential = np.cos(self.angle_to_target)
+
+    def calc_env_state(self, action):
+        if not np.isfinite(self.robot_state).all():
+            print("~INF~", self.robot_state)
+            self.done = True
+
+        self.calc_base_reward(action)
+
+    def calc_base_reward(self, action):
+
+        # Bookkeeping stuff
+        old_linear_potential = self.linear_potential
+        old_angular_potential = self.angular_potential
+
+        self.calc_potential()
+
+        if self.distance_to_target < 1:
+            self.add_angular_progress = False
+
+        linear_progress = self.linear_potential - old_linear_potential
+        angular_progress = self.angular_potential - old_angular_potential
+
+        self.progress = linear_progress
+        # if self.add_angular_progress:
+        #     self.progress += 100 * angular_progress
+
+        self.posture_penalty = 0
+        if not -0.2 < self.robot.body_rpy[1] < 0.4:
+            self.posture_penalty = abs(self.robot.body_rpy[1])
+
+        if not -0.4 < self.robot.body_rpy[0] < 0.4:
+            self.posture_penalty += abs(self.robot.body_rpy[0])
+
+        self.energy_penalty = self.electricity_cost * float(
+            np.abs(action * self.robot.joint_speeds).mean()
+        )
+        self.energy_penalty += self.stall_torque_cost * float(np.square(action).mean())
+
+        self.joints_penalty = float(
+            self.joints_at_limit_cost * self.robot.joints_at_limit
+        )
+
+        # Calculate done
+        height = self.robot.body_xyz[2] - np.min(self.robot.feet_xyz[:, 2])
+        self.tall_bonus = 2.0 if height > self.termination_height else -1.0
+        self.done = self.done or self.tall_bonus < 0
 
     def calc_local_height_map(self):
         x, y, z = self.robot.body_xyz
@@ -772,3 +866,5 @@ class Walker3DTerrainEnv(EnvBase):
         vision_map = self.terrain.data.reshape(tx, ty)[
             x - vs : x + vs + 1, y - vs : y + vs + 1
         ]
+
+        return vision_map
